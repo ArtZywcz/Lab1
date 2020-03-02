7ff3055aab15b0e44a3913cd29d5471ea818dc5a
Artur Żywczak <artzywcz@gmail.com>
2020-03-02 21:59:57 +0100

    Chyba działa

---
+++ ConsoleApp1/ConsoleApp1/DB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    public class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var query = "SELECT * FROM Klienci";
+
+            var command = new SqlCommand(query, connection);
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetString(1)}");
+                }
+            }
+        }
+
+        public void Insert(SqlConnection connection, string id, string compName)
+        {
+            var query = "INSERT INTO Klienci(IDklienta, NazwaFirmy) VALUES(@id, @compName)";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@compName", compName);
+
+            command.ExecuteNonQuery();
+
+        }
+
+        public void Delete(SqlConnection connection, string id)
+        {
+            var query = "DELETE FROM Klienci WHERE IDklienta = @id";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            command.ExecuteNonQuery();
+
+        }
+
+        public void Update(SqlConnection connection, string id, string compName)
+        {
+            var query = "UPDATE Klienci SET NazwaFirmy = @compName WHERE IDklienta = @id";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@compName", compName);
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source=DESKTOP-M1MKO1J\SQLEXPRESS01;Initial Catalog=BD1d_cz_1B;Integrated Security=True";
+
+            var connection = new SqlConnection(connectionString);
+
+            var db = new DB();
+            connection.Open();
+
+            db.Select(connection);
+            Console.ReadLine();
+
+            db.Insert(connection, "NOWYY", "Testowy Tester");
+            db.Select(connection);
+            Console.ReadLine();
+            
+            db.Update(connection, "NOWYY", "Probny Probownik");
+            db.Select(connection);
+            Console.ReadLine();
+
+            db.Delete(connection, "NOWYY");
+            db.Select(connection);
+            Console.ReadLine();
+
+            connection.Close();
+
+        }
+    }
+}
